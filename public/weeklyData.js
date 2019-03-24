d4cf64dcc1692dbed4041880a147de1c4cd7b0e8
Raymond Qu <raymond101.qu@gmail.com>
2019-03-24 04:07:02 -0400

    fixed up charts and labelling

--- public/weeklyData.js
+++ public/weeklyData.js
@@ -1,11 +1,12 @@
 // replace these static values
-
+let weeklyData = [20, 10, 5, 2, 20, 30, 45];
+let pastWeekData = [60, 80, 90, 10, 20, 50, 20];
 
 function toLitres(value){
   return (value) + " ml";
 }
 
-var ctx = document.getElementById('dailyScoreChart').getContext('2d');
+var ctx = document.getElementById('weeklyData').getContext('2d');
 var chart = new Chart(ctx, {
   // The type of chart we want to create
   type: 'line',
@@ -16,13 +17,13 @@ var chart = new Chart(ctx, {
     datasets: [
       {
         label: 'Water Intake This Week',
-        data: [20, 10, 5, 2, 20, 30, 45],
+        data: [weeklyData[0], weeklyData[1], weeklyData[2], weeklyData[3], weeklyData[4], weeklyData[5], weeklyData[6]],
         borderColor: 'rgba(75, 192, 192, 1)',
         backgroundColor: 'rgba(75, 192, 192, 0.2)',
       },
       {
         label: 'Water Intake Last Week',
-        data: [60, 80, 90, 10, 20, 50, 20],
+        data: [pastWeekData[0], pastWeekData[1], pastWeekData[2], pastWeekData[3], pastWeekData[4], pastWeekData[5], pastWeekData[6]],
         borderColor: 'rgba(192, 192, 192, 1)',
         backgroundColor: 'rgba(192, 192, 192, 0.2)',
       }
